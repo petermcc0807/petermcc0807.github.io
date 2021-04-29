ccca29f474ea0e0d49ff7b798f32f1a3045dc0df
Peter McCarthy <peter.mccarthy.0807@googlemail.com>
2021-04-29 12:57:00 +0100

    Created.

---
+++ js/index.js
@@ -0,0 +1,93 @@
+const main = () =>
+{
+    window.addEventListener('load', () =>
+    {
+        if ('serviceWorker' in navigator)
+        {
+            navigator.serviceWorker.register('/service-worker.js').then(registration =>
+            {
+                console.log('navigator.serviceWorker.register(): service worker registered');
+
+                if (registration.active != null)
+                {
+                    registration.addEventListener('updatefound', () =>
+                    {
+                        console.log('registration.addEventListener(updatefound): update found');
+
+                        const installingServiceWorker = registration.installing;
+
+                        if (installingServiceWorker != null)
+                        {
+                            installingServiceWorker.addEventListener('statechange', () =>
+                            {
+                                if (installingServiceWorker.state === 'installed')
+                                {
+                                    console.log('installingServiceWorker.addEventListener(statechange): update ready to install');
+
+                                    // Do something
+
+                                    if ('Notification' in window)
+                                    {
+                                        const notify = () =>
+                                        {
+                                            const notification = new Notification('APStore: Update installed');
+
+                                            location.reload();
+                                        };
+
+                                        if (Notification.permission === 'granted')
+                                            notify();
+                                        else
+                                        {
+                                            Notification.requestPermission().then((permission) =>
+                                            {
+                                                if (Notification.permission === 'granted')
+                                                    notify();
+                                            });
+                                        }
+                                    }
+                                }
+                            });
+
+                            // Do something
+                        }
+                    });
+                }
+
+                const timerId = setInterval(() => { const promise = registration.update(); }, 15000);
+
+                // Do something
+            }).catch(error =>
+            {
+                console.log('navigator.serviceWorker.register(): service worker not registered;', error);
+
+                // Do something
+            });
+
+            // Do something
+        }
+
+        // const socket = io('http://localhost:10241');
+        const socket = io('https://localhost:10241');
+
+        socket.on('pong', (data) =>
+        {
+            console.log(`socket.on(pong): id=${ socket.id }, data=${ JSON.stringify(data) }`);
+
+            // Do something
+        });
+
+        const button = document.getElementById('PingButton');
+
+        button.onclick = () =>
+        {
+            // Do something
+
+            socket.emit('ping', { time: Date.now() });
+        };
+    });
+
+    // Do something
+};
+
+main();
---
+++ service-worker.js
@@ -0,0 +1,73 @@
+//
+// NOTE [1] : If hosting in Github Pages (cannot disable caching), delete local cache to force refresh
+//
+//      [2] : https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+//
+
+const VERSION = '0.0.7';
+
+const CACHE_NAME = 'apstore';
+
+const ASSETS =
+[
+    '/',
+    '/index.html',
+    '/img/logo-512x512.png',
+    '/js/index.js',
+    '/js/socket.io/socket.io.js',
+    '/js/socket.io/socket.io.js.map'
+];
+
+self.addEventListener('install', event =>
+{
+    console.log('self.addEventListener(install)');
+
+    // NOTE: see [1]
+    caches.delete(CACHE_NAME);
+    // NOTE
+
+    self.skipWaiting();
+
+    event.waitUntil(caches.open(CACHE_NAME).then(cache =>
+    {
+        cache.addAll(ASSETS);
+
+        console.log('self.addEventListener(install): assets cached');
+
+        // Do something
+    }).catch(error =>
+    {
+        console.log('self.addEventListener(install): event.waitUntil().catch();', error);
+
+        // Do something
+    }));
+});
+
+self.addEventListener('activate', event =>
+{
+    console.log('self.addEventListener(activate)');
+
+    // Do something
+});
+
+self.addEventListener('fetch', event =>
+{
+    console.log('self.addEventListener(fetch)');
+
+    event.respondWith(caches.match(event.request).then(response =>
+    {
+        let promise;
+
+        if (response !== undefined)
+            promise = response;
+        else
+            promise = fetch(event.request);
+
+        return promise;
+    }).catch(error =>
+    {
+        console.log('self.addEventListener(fetch): event.respondWith().catch();', error);
+
+        // Do something
+    }));
+});
