cfa79b7ab912556e25577cc8443459a826d1fe64
Peter McCarthy <71627598+petermcc0807@users.noreply.github.com>
2021-05-01 18:16:30 +0100

    Changed.

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // NOTE [1] : https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
 //
 
-const VERSION = '0.0.2';
+const VERSION = '0.0.3';
 
 const CACHE_NAME = 'apstore';
 
