05203e9a8e1443a1b621d0e7dfc92c56e5030961
Peter McCarthy <peter.mccarthy.0807@googlemail.com>
2021-05-04 09:15:54 +0100

    Changed.

--- js/index.js
+++ js/index.js
@@ -90,7 +90,7 @@ const main = () =>
         const webSocket = new WebSocket('ws://localhost:10242');
         webSocket.addEventListener('message', (event) =>
         {
-            console.log('webSocket.addEventListener(message): data =', event.data);
+            console.log('webSocket.addEventListener(message): event.data=' + event.data);
         });
         // TEMPORARY
 
