a670bfd8975ccc81242088eed1c6232a1c5c5e9e
Peter McCarthy <peter.mccarthy.0807@googlemail.com>
2021-05-11 11:05:38 +0100

    Changed.

--- js/index.ws.js
+++ js/index.ws.js
@@ -2,19 +2,53 @@ const main = () =>
 {
     window.addEventListener('load', () =>
     {
-        const webSocket = new WebSocket('ws://localhost:10242');
-
-        webSocket.addEventListener('message', (event) =>
-        {
-            console.log('webSocket.addEventListener(message): event.data=' + event.data);
-        });
+        let webSocket;
 
         const button = document.getElementById('PingButton');
 
         button.onclick = () =>
         {
-            webSocket.send('{\"time\":' + Date.now() + '}');
+            try { webSocket.send('{\"time\":' + Date.now() + '}'); }
+
+            catch (exception) { }
         };
+
+        button.disabled = true;
+
+        const connect = () =>
+        {
+            webSocket = new WebSocket('ws://localhost:10242');
+
+            webSocket.addEventListener('open', (event) =>
+            {
+                console.log('webSocket.addEventListener(open)');
+
+                button.disabled = false;
+            });
+
+            webSocket.addEventListener('close', (event) =>
+            {
+                console.log('webSocket.addEventListener(close)');
+
+                button.disabled = true;
+
+                const timerId = setTimeout(() => connect(), 2500);
+            });
+
+            webSocket.addEventListener('message', (event) =>
+            {
+                console.log(`webSocket.addEventListener(message): event.data=${ event.data }`);
+            });
+
+            webSocket.addEventListener('error', (error) =>
+            {
+                console.log('webSocket.addEventListener(error)');
+
+                webSocket.close();
+            });
+        };
+
+        connect();
     });
 };
 
