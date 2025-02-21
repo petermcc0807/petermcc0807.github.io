14802c670f5b3eb6692faae347c11e8c052220d1
petermcc0807 <peter.mccarthy.0807@googlemail.com>
2025-02-21 08:53:00 +0000

    Changed.

--- js/index.js
+++ js/index.js
@@ -6,6 +6,10 @@ const main = () =>
 {
     const intervalId = setInterval(async () =>
     {
+        const userAgent = window.navigator.userAgent;
+
+        console.log(`setInterval(): userAgent=${ userAgent }`);
+
         if (chrome.runtime !== undefined)
         {
             const response = await chrome.runtime.sendMessage(CHROME_EXTENSION, { sender: 'launcher', type: 'close', data: { } });
