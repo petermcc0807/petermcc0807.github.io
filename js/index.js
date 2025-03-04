089fe2591142af511ab25812c15594ae3c14dfb2
petermcc0807 <peter.mccarthy.0807@googlemail.com>
2025-03-04 16:12:00 +0000

    Changed / deleted.

--- js/index.js
+++ js/index.js
@@ -1,24 +1,8 @@
 'use strict';
 
-const CHROME_EXTENSION = 'conmonkkabbmjdkfbmfiinlfnjbcbolc';
-
 const main = () =>
 {
-    const intervalId = setInterval(async () =>
-    {
-        const userAgent = window.navigator.userAgent;
-
-        console.log(`setInterval(): userAgent=${ userAgent }`);
-
-        if (chrome.runtime !== undefined)
-        {
-            const response = await chrome.runtime.sendMessage(CHROME_EXTENSION, { sender: 'launcher', type: 'close', data: { } });
-
-            console.log(`chrome.runtime.sendMessage(): sender=${ response.sender }, type=${ response.type }`);
-
-            // Do something
-        }
-    }, 1000);
+    // Do something
 }
 
 main();
