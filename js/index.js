b74fca27790fd2201adfa8440be6308787994fc8
petermcc0807 <peter.mccarthy.0807@googlemail.com>
2025-03-06 10:27:22 +0000

    Changed.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
 'use strict';
 
-const main = () =>
+const main = async () =>
 {
-    // Do something
+    const filters = [ { services: [ '0000180D-0000-1000-8000-00805F9B34FB' ] }];
+
+    let devices;
+
+    console.log('main(): Getting BLE devices...');
+
+    try
+    {
+
+        devices = await navigator.bluetooth.requestDevice({ filters });
+
+        console.log('main(): got BLE devices');
+
+        // Do something
+    }
+
+    catch (exception) { }
 }
 
 main();
